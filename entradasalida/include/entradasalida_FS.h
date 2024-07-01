f399ef1cbde2a23f4b237ab7a22bd440a43aa107
Gonzalo Gomez <luisgomez5847@gmail.com>
2024-07-01 20:56:30 +0000

    headers FS

---
+++ entradasalida/include/entradasalida_FS.h
@@ -0,0 +1,15 @@
+#ifndef ENTRADASALIDA_FS_H_
+#define ENTRADASALIDA_FS_H_
+
+#include "../../utils/include/utils.h"
+#include "../../utils/include/conexiones.h"
+
+extern t_log* logger;
+
+extern uint32_t TIEMPO_UNIDAD_TRABAJO;
+extern char* PATH_BASE_DIALFS;
+extern uint32_t BLOCK_SIZE;
+extern uint32_t BLOCK_COUNT;
+extern uint32_t RETRASO_COMPACTACION;
+
+#endif //ENTRADASALIDA_FS_H_
---
+++ entradasalida/src/entradasalida_FS.c
@@ -0,0 +1,2 @@
+#include "../include/entradasalida_FS.h"
+
