77d6fd6ac7a8b2b2010578d9e78a073cfbce83eb
Gonzalo Gomez <luisgomez5847@gmail.com>
2024-07-02 19:57:04 +0000

    correciones a crear_proceso()

--- memoria/src/memKernel.c
+++ memoria/src/memKernel.c
@@ -31,14 +31,13 @@ void conexion_con_kernel(){
 }
 
 void crear_proceso(){ // llega el pid y el path de instrucciones
-    uint32_t *sizeTotal = malloc(sizeof(uint32_t));
-    uint32_t *desplazamiento = malloc(sizeof(int));
-    *desplazamiento = 0;
-    void* buffer= recibir_buffer(sizeTotal,socket_kernel_memoria);
+    uint32_t sizeTotal;
+    uint32_t desplazamiento = 0;
+    void* buffer= recibir_buffer(&sizeTotal, socket_kernel_memoria);
 
     if (buffer != NULL) {
-        uint32_t PID = leer_de_buffer_uint32(buffer,desplazamiento);
-        char* path_parcial = leer_de_buffer_string(buffer,desplazamiento);
+        uint32_t PID = leer_de_buffer_uint32(buffer, &desplazamiento);
+        char* path_parcial = leer_de_buffer_string(buffer, &desplazamiento);
 
         log_info(logger_debug,"Llego un proceso para cargar: PID: %u  Direccion: %s",PID,path_parcial);
         
@@ -47,8 +46,10 @@ void crear_proceso(){ // llega el pid y el path de instrucciones
         usleep(retardo*1000);
 
         if(creado){
+            log_info(logger, "Proceso cargado con exito, PID: %u", PID);
             enviar_instruccion_con_PID_por_socket(CARGA_EXITOSA_PROCESO, PID, socket_kernel_memoria);
         }else{
+            log_info(logger, "Falla al cargar un proceso, PID: %u", PID);
             enviar_instruccion_con_PID_por_socket(ERROR_AL_CARGAR_EL_PROCESO, PID, socket_kernel_memoria);
         }  
 
@@ -56,8 +57,6 @@ void crear_proceso(){ // llega el pid y el path de instrucciones
         // Manejo de error en caso de que recibir_buffer devuelva NULL
         log_error(logger_debug,"Error al recibir el buffer");
     }
-    free(sizeTotal);
-    free(desplazamiento);
     free(buffer);
 }
 
