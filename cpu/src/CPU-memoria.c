98deab9109a6b98c08e5b8bf420d92de7236d626
Gonzalo Gomez <luisgomez5847@gmail.com>
2024-07-11 23:39:59 +0000

    Fix a gestion_conexion_memoria

--- cpu/src/CPU-memoria.c
+++ cpu/src/CPU-memoria.c
@@ -12,6 +12,10 @@ void gestionar_conexion_memoria()
     while (continuar_iterando)
     {
         operacion = recibir_operacion(socket_cpu_memoria);
+
+        uint32_t size;
+        uint32_t desplazamiento;
+        void* buffer;
         switch (operacion)
         {
         case MENSAJE:
@@ -31,9 +35,8 @@ void gestionar_conexion_memoria()
             break;
         
         case SOLICITUD_COPY_STRING_READ:
-            uint32_t size;
-            uint32_t desplazamiento = 0;
-            void* buffer = recibir_buffer(&size, socket_cpu_memoria);
+            desplazamiento = 0;
+            buffer = recibir_buffer(&size, socket_cpu_memoria);
             string_leida_de_memoria = leer_de_buffer_string(buffer, &desplazamiento);
             log_info(logger_debug, "Llego la string \'%s\' de memoria", string_leida_de_memoria);
             sem_post(&respuesta_copy_string);
@@ -41,14 +44,13 @@ void gestionar_conexion_memoria()
             break;
 
         case SOLICITUD_MOV_IN:
-            uint32_t size;
-            uint32_t desplazamiento = 0;
-            void* buffer = recibir_buffer(&size, socket_cpu_memoria);
+            desplazamiento = 0;
+            buffer = recibir_buffer(&size, socket_cpu_memoria);
 
             uint32_t tamanio = leer_de_buffer_uint32(&buffer, &desplazamiento);
             uint32_t respuesta = leer_de_buffer_uint32(&buffer, &desplazamiento);
 
-            if (tamanio = sizeof(uint8_t))
+            if (tamanio == sizeof(uint8_t))
                 {respuesta_mov_in_8 = respuesta;}
             else {respuesta_mov_in_32 = respuesta;}
             
